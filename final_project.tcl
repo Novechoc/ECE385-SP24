72efd0b5978b476c1e1f54e838685c54772b61e4
Yuxi <yuxi.c@hotmail.com>
2024-04-25 14:43:11 -0500

    add background

--- final_project.tcl
+++ final_project.tcl
@@ -20,6 +20,7 @@ proc checkRequiredFiles { origin_dir} {
   set files [list \
  "[file normalize "$origin_dir/vivado_project/final_project.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.xci"]"\
  "[file normalize "$origin_dir/vivado_project/final_project.srcs/sources_1/ip/hdmi_tx_0/hdmi_tx_0.xci"]"\
+ "[file normalize "$origin_dir/vivado_project/final_project.srcs/sources_1/ip/background_rom/background_rom.xci"]"\
  "[file normalize "$origin_dir/vivado_project/final_project.srcs/utils_1/imports/synth_1/mb_usb_hdmi_top.dcp"]"\
   ]
   foreach ifile $files {
@@ -32,13 +33,15 @@ proc checkRequiredFiles { origin_dir} {
   set files [list \
  "[file normalize "$origin_dir/src/design/Color_Mapper.sv"]"\
  "[file normalize "$origin_dir/src/design/VGA_controller.sv"]"\
+ "[file normalize "$origin_dir/src/tool/Image_to_COE/background/background_example.sv"]"\
+ "[file normalize "$origin_dir/src/tool/Image_to_COE/background/background_palette.sv"]"\
  "[file normalize "$origin_dir/src/design/ball.sv"]"\
  "[file normalize "$origin_dir/src/design/hex_driver.sv"]"\
  "[file normalize "$origin_dir/src/design/knife.sv"]"\
  "[file normalize "$origin_dir/src/design/logic.sv"]"\
- "[file normalize "$origin_dir/src/design/ram.sv"]"\
  "[file normalize "$origin_dir/src/design/world_map.sv"]"\
  "[file normalize "$origin_dir/src/design/mb_usb_hdmi_top.sv"]"\
+ "[file normalize "$origin_dir/src/tool/Image_to_COE/background/background.COE"]"\
  "[file normalize "$origin_dir/vivado_project/final_project.srcs/constrs_1/imports/pin_assignment/mb_usb_hdmi_top.xdc"]"\
   ]
   foreach ifile $files {
@@ -174,12 +177,12 @@ set_property -name "simulator.xsim_gcc_version" -value "6.2.0" -objects $obj
 set_property -name "simulator.xsim_version" -value "2022.2" -objects $obj
 set_property -name "simulator_language" -value "Mixed" -objects $obj
 set_property -name "sim_compile_state" -value "1" -objects $obj
-set_property -name "webtalk.activehdl_export_sim" -value "2" -objects $obj
-set_property -name "webtalk.modelsim_export_sim" -value "2" -objects $obj
-set_property -name "webtalk.questa_export_sim" -value "2" -objects $obj
-set_property -name "webtalk.riviera_export_sim" -value "2" -objects $obj
-set_property -name "webtalk.vcs_export_sim" -value "2" -objects $obj
-set_property -name "webtalk.xsim_export_sim" -value "2" -objects $obj
+set_property -name "webtalk.activehdl_export_sim" -value "6" -objects $obj
+set_property -name "webtalk.modelsim_export_sim" -value "6" -objects $obj
+set_property -name "webtalk.questa_export_sim" -value "6" -objects $obj
+set_property -name "webtalk.riviera_export_sim" -value "6" -objects $obj
+set_property -name "webtalk.vcs_export_sim" -value "6" -objects $obj
+set_property -name "webtalk.xsim_export_sim" -value "6" -objects $obj
 set_property -name "xpm_libraries" -value "XPM_CDC XPM_FIFO XPM_MEMORY" -objects $obj
 
 # Create 'sources_1' fileset (if not found)
@@ -201,13 +204,15 @@ set obj [get_filesets sources_1]
 set files [list \
  [file normalize "${origin_dir}/src/design/Color_Mapper.sv"] \
  [file normalize "${origin_dir}/src/design/VGA_controller.sv"] \
+ [file normalize "${origin_dir}/src/tool/Image_to_COE/background/background_example.sv"] \
+ [file normalize "${origin_dir}/src/tool/Image_to_COE/background/background_palette.sv"] \
  [file normalize "${origin_dir}/src/design/ball.sv"] \
  [file normalize "${origin_dir}/src/design/hex_driver.sv"] \
  [file normalize "${origin_dir}/src/design/knife.sv"] \
  [file normalize "${origin_dir}/src/design/logic.sv"] \
- [file normalize "${origin_dir}/src/design/ram.sv"] \
  [file normalize "${origin_dir}/src/design/world_map.sv"] \
  [file normalize "${origin_dir}/src/design/mb_usb_hdmi_top.sv"] \
+ [file normalize "${origin_dir}/src/tool/Image_to_COE/background/background.COE"] \
 ]
 add_files -norecurse -fileset $obj $files
 
@@ -218,6 +223,16 @@ set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
 
 set file "$origin_dir/src/design/VGA_controller.sv"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
+
+set file "$origin_dir/src/tool/Image_to_COE/background/background_example.sv"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
+
+set file "$origin_dir/src/tool/Image_to_COE/background/background_palette.sv"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
@@ -238,11 +253,6 @@ set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
 
 set file "$origin_dir/src/design/logic.sv"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
-
-set file "$origin_dir/src/design/ram.sv"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
@@ -300,6 +310,27 @@ set added_files [add_files -fileset sources_1 $files]
 
 # Set 'sources_1' fileset file properties for local files
 set file "hdmi_tx_0/hdmi_tx_0.xci"
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "generate_files_for_reference" -value "0" -objects $file_obj
+set_property -name "registered_with_manager" -value "1" -objects $file_obj
+if { ![get_property "is_locked" $file_obj] } {
+  set_property -name "synth_checkpoint_mode" -value "Singular" -objects $file_obj
+}
+
+
+# Set 'sources_1' fileset object
+set obj [get_filesets sources_1]
+# Add local files from the original project (-no_copy_sources specified)
+set files [list \
+ [file normalize "${origin_dir}/vivado_project/final_project.srcs/sources_1/ip/background_rom/background_rom.xci" ]\
+]
+set added_files [add_files -fileset sources_1 $files]
+
+# Set 'sources_1' fileset file properties for remote files
+# None
+
+# Set 'sources_1' fileset file properties for local files
+set file "background_rom/background_rom.xci"
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "generate_files_for_reference" -value "0" -objects $file_obj
 set_property -name "registered_with_manager" -value "1" -objects $file_obj
@@ -731,6 +762,7 @@ if { $obj != "" } {
 
 }
 set obj [get_runs synth_1]
+set_property -name "needs_refresh" -value "1" -objects $obj
 set_property -name "part" -value "xc7s50csga324-1" -objects $obj
 set_property -name "incremental_checkpoint" -value "$proj_dir/final_project.srcs/utils_1/imports/synth_1/mb_usb_hdmi_top.dcp" -objects $obj
 set_property -name "auto_incremental_checkpoint" -value "1" -objects $obj
