4d58fa9621ba1d36256ded1396510c28d0867895
Yuxi <yuxi.c@hotmail.com>
2024-04-27 21:05:32 -0500

    add door, moving ground, obstacle

--- final_project.tcl
+++ final_project.tcl
@@ -883,6 +883,7 @@ if { $obj != "" } {
 
 }
 set obj [get_runs synth_1]
+set_property -name "needs_refresh" -value "1" -objects $obj
 set_property -name "part" -value "xc7s50csga324-1" -objects $obj
 set_property -name "incremental_checkpoint" -value "$proj_dir/final_project.srcs/utils_1/imports/synth_1/mb_usb_hdmi_top.dcp" -objects $obj
 set_property -name "auto_incremental_checkpoint" -value "1" -objects $obj
@@ -1107,6 +1108,7 @@ set_property -name "options.warn_on_violation" -value "1" -objects $obj
 
 }
 set obj [get_runs impl_1]
+set_property -name "needs_refresh" -value "1" -objects $obj
 set_property -name "part" -value "xc7s50csga324-1" -objects $obj
 set_property -name "strategy" -value "Vivado Implementation Defaults" -objects $obj
 set_property -name "steps.write_bitstream.args.readback_file" -value "0" -objects $obj
